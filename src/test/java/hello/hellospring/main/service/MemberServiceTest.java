bbff6fa7d9bbc2f6376f224c10257ace5a6c0af1
VerdureKo <verdure.ko@gmail.com>
2023-08-03 12:08:33 +0900

    import수정

--- src/test/java/hello/hellospring/main/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/main/service/MemberServiceTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
--- src/test/java/hello/hellospring/team/service/TeamServiceTest.java
+++ src/test/java/hello/hellospring/team/service/TeamServiceTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
